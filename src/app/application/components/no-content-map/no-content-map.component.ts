a76725490e80f904a0da7507a14692cf5616fcf8
Mustafa Tokay <se.mustafatokay@gmail.com>
2023-08-29 22:01:58 +0300

    no-content-map component added

---
+++ src/app/application/components/no-content-map/no-content-map.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-application-no-content-map',
+  templateUrl: './no-content-map.component.html',
+  styleUrls: ['../../../app.component.css', './no-content-map.component.css']
+})
+export class NoContentMap {
+
+  constructor(
+
+  ) {}
+
+}
