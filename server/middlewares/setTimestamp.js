8c034f4e44be9735e4e487b2e173cd6f796f5e15
Mustafa Tokay <se.mustafatokay@gmail.com>
2023-09-11 22:58:30 +0300

    date format changes and log remove

--- server/middlewares/setTimestamp.js
+++ server/middlewares/setTimestamp.js
@@ -1,45 +1,42 @@
+const errorService = require('../services/error-service');
+const tzoffset = (new Date()).getTimezoneOffset() * 60000;
 
 module.exports = (req, res, next) => {
   try {
     // Creating Mode
     if (req.method == 'POST') {
-      console.log('INSERTED');
       if (req.body.requestData ?? false) {
         const parsedRequest = JSON.parse(req.body.requestData);
-        parsedRequest.createdAt = new Date().toISOString().slice(0, 19);
+        parsedRequest.createdAt = (new Date(Date.now() - tzoffset)).toISOString().slice(0, 19).replace(/[^0-9:-]/g, ' ');
         parsedRequest.createdBy = req.userData.userId;  
         req.body.requestData = JSON.stringify(parsedRequest);
       } else {
-        req.body.createdAt = new Date().toISOString().slice(0, 19);
+        req.body.createdAt = (new Date(Date.now() - tzoffset)).toISOString().slice(0, 19).replace(/[^0-9:-]/g, ' ');
         req.body.createdBy = req.userData.userId;
       }
 
     // Updating Mode
     } else if(req.method == 'PUT') {
-      console.log('UPDATED');
       if (req.body.requestData ?? false) {
-        console.log('requestData ile geldik');
         const parsedRequest = JSON.parse(req.body.requestData);
-        parsedRequest.updatedAt = new Date().toISOString().slice(0, 19);
+        parsedRequest.updatedAt = (new Date(Date.now() - tzoffset)).toISOString().slice(0, 19).replace(/[^0-9:-]/g, ' ');
         parsedRequest.updatedBy = req.userData.userId;
-        parsedRequest.createdAt ? parsedRequest.createdAt.slice(0, 19) : null;
+
         req.body.requestData = JSON.stringify(parsedRequest);
-        console.log(req.body.requestData);
-        console.log('-----------------------------');
 
 
       } else {
-        console.log('sadece body ile geldik');
-        req.body.updatedAt = new Date().toISOString().slice(0, 19);
+        req.body.updatedAt = (new Date(Date.now() - tzoffset)).toISOString().slice(0, 19).replace(/[^0-9:-]/g, ' ');
         req.body.updatedBy = req.userData.userId;
-        req.body.createdAt ? req.body.createdAt.slice(0, 19) : null;
-        //console.log(req.body);
-        //console.log('-----------------------------');
       }
     }
   } catch (error) {
-    console.log(error)
+      errorService.handleError(
+        errorService.errors.SERVICE_ERROR_SETTIMESTAMP.code,
+        errorService.errors.SERVICE_ERROR_SETTIMESTAMP.message,
+        error
+      );      
   } finally {
-    return next();
+      return next();
   }
 };
