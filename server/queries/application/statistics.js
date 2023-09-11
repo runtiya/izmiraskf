cc26977b9c7396f74bb5c2d48ceab22edd0517fa
Mustafa Tokay <se.mustafatokay@gmail.com>
2023-09-11 22:59:15 +0300

    statistics models view name changed to global

--- server/queries/application/statistics.js
+++ server/queries/application/statistics.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "getTeamsCountByTown" : "select * from view_admin_statistics_teamscountbytown" ,
-  "getStadiumsCountByTown" : "select * from view_admin_statistics_stadiumscountbytown",
-  "getStadiumsCountByFloorType" : "select * from view_admin_statistics_stadiumscountbyfloortype",
+  "getTeamsCountByTown" : "select * from view_global_statistics_teamscountbytown" ,
+  "getStadiumsCountByTown" : "select * from view_global_statistics_stadiumscountbytown",
+  "getStadiumsCountByFloorType" : "select * from view_global_statistics_stadiumscountbyfloortype",
   "getMatchStatusCountByLeague" : "select * from view_global_statistics_matchstatuscountbyleague where seasonid = ?"
 }
