d842ca7003c7fd61574d615e75d3571fc99cd0c1
oguztimurtasdelen <oguztasdelen96@gmail.com>
2024-02-01 13:12:57 +0300

    environment variables changed for demo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   development: false,
-  serverUrl: "http://localhost:3000/",
-  clientUrl: "http://localhost:4200"
-  //serverUrl: "http://www.runtiya.online:3000/",
-  //clientUrl: "http://www.runtiya.online"
+  //serverUrl: "http://localhost:3000/",
+  //clientUrl: "http://localhost:4200"
+  serverUrl: "http://www.runtiya.online:3000/",
+  clientUrl: "http://www.runtiya.online"
 };
