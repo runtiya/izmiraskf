4f7399e480850325f82f6ab20d9af10e71b09c85
Mustafa Tokay <se.mustafatokay@gmail.com>
2024-03-02 11:53:14 +0300

    server ssl adjustment

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const app = require('./app');
 const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const debug = require('debug')('node-angular');
 
 
@@ -42,10 +44,19 @@ const onListening = () => {
   console.log("server is onListening")
 }
 
+
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
+var server;
+if (process.env.NODE_ENV == "PROD") {
+  server = https.createServer({
+    key: fs.readFileSync('/etc/ssl/private.key'),
+    cert: fs.readFileSync('/etc/ssl/certificate.crt'),
+  }, app);
+} else {
+  server = http.createServer(app);
+}
 
-const server = http.createServer(app);
 server.on("error", onError);
 server.on("listening", onListening);
 server.listen(port);
